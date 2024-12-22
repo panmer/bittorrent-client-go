c00ca0ce52239ec56aea2232358d9daf32c08c32
trungvuive <trung150502@gmail.com>
2024-12-22 15:30:56 +0700

    add torrent response format

--- torrent.go
+++ torrent.go
@@ -3,7 +3,7 @@ package torrent
 type InfoData struct {
 	Length int `bencode:"length"`
 	Name string `bencode:"name"`
-	Piece_length int `bencode:"piece length"`
+	PieceLength int `bencode:"piece length"`
 	Pieces string `bencode:"pieces"`
 }
 type Torrent struct {
@@ -14,4 +14,12 @@ type Torrent struct {
 type TrackerResponse struct {
 	Peers string `bencode:"peers"`
 	Interval int`bencode:"interval"`
+}
+
+type TCPRequest struct {
+	Length   uint8
+	Protocol [19]byte
+	Reserve  [8]byte
+	InfoHash [20]byte
+	PeerID   [20]byte
 }
