fd7f3325e740058b58fe7f323dfe081558f36601
trungvuive <trung150502@gmail.com>
2024-12-22 10:14:35 +0700

    rename files

---
+++ decode/command.go
@@ -0,0 +1,26 @@
+package decode
+
+import (
+	"fmt"
+	"strings"
+	// "encoding/json"
+	"github.com/jackpal/bencode-go"
+)
+
+func decodeBencode(bencodedString string) (interface {}, error) {
+	fmt.Println(bencodedString)
+	data, err := bencode.Decode(strings.NewReader(bencodedString))
+	return data, err
+}
+
+func Command(bencodedValue string) {
+	// bencodedString := "Encode Command"
+	_, err := decodeBencode(bencodedValue)
+
+	if err != nil {
+		fmt.Println("Error decoding bencoded value:", err);
+		return
+	}
+
+	fmt.Println("vuive");
+}
